cb7af8f484c1a228cff8190ac4552148f169e58e
lishihong <lieric7766@gmail.com>
2018-09-06 03:11:45 +0800

    update network_grid

--- nn.cpp
+++ nn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "nn.h"
 
@@ -7,8 +8,13 @@
 NN::NN()
 {}
 
-void NN::network_grid() {
-	//
+void NN::network_grid(std::vector<int> v, std::string activate) {
+	std::vector<std::vector<std::vector<int> > > all_weight_grids;
+	for (std::vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter) {
+		std::vector<std::vector<int> > weight_grid(v[iter], std::vector<int>(v[iter]+1));
+		all_weight_grids.push_back(weight_grid); 
+	}	
+
 }
 
 void NN::load_model() {
--- nn.h
+++ nn.h
@@ -1,10 +1,13 @@
 #ifndef NN_H
 #define NN_H
 
+#include <vector>
+#include <string>
+
 class NN {
 	public:
 		NN();
-		void network_grid();
+		void network_grid(std::vector<int>, std::string);
 		void load_model();
 		void output_model(); 
 		double predict();
@@ -14,6 +17,8 @@ class NN {
 		void train();
 		double numerical_gradient();
 		double gradient_descent();
+		double sigmoid();
+		double relu();
 };
 
 #endif
